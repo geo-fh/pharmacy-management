dc080e27ce2dedad9d601ed1554ce59aead54fa6
geo-fh <georgesifarhat@gmail.com>
2022-06-18 11:04:08 +0300

    grab user info from database

--- assets/js/patientinfo.js
+++ assets/js/patientinfo.js
@@ -3,8 +3,15 @@ var patientid = 1;
 xhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
     var info = JSON.parse(this.responseText);
-    var infolist = 'patient_id: ' + info[0].patient_id + '\nuser_id: ' + info[0].user_id + '\nname: ' + info[0].first_name + ' ' + info[0].last_name + '\nbirthdate: ' + info[0].date_of_birth + '\naddress: ' + info[0].address + '\nmobile_number: ' + info[0].mobile_number;
-    alert(infolist);
+    document.getElementById("first_name").value = info[0].first_name;
+    document.getElementById("last_name").value = info[0].last_name;
+    document.getElementById("email").value = info[0].email_address;
+    document.getElementById("mobile").value = info[0].mobile_number;
+    document.getElementById("birthdate").value = info[0].date_of_birth;
+    document.getElementById("city").value = 'Beirut';
+    document.getElementById("address").value = info[0].address;
+    document.getElementById("nav_name").innerText = info[0].first_name + ' ' + info[0].last_name;
+
 }
 };
 xhttp.open("POST", "assets/php/selectFromPatient.php", true);
--- assets/php/selectFromPatient.php
+++ assets/php/selectFromPatient.php
@@ -6,7 +6,7 @@ if (isset($_POST["patientid"])) {
     $patientid = $_POST["patientid"];
 
     $dbquery = $con->query(
-        "SELECT * FROM patient WHERE patient_id = '$patientid'"
+        "SELECT * FROM patient JOIN user on patient.user_id = user.user_id WHERE patient_id = '$patientid'"
     );
     $result = mysqli_fetch_all($dbquery, MYSQLI_ASSOC);
     echo json_encode($result);
