6360df0d4e45bba9d43ffbcf397789782c4404c2
geo-fh <georgesifarhat@gmail.com>
2022-06-18 10:34:53 +0300

    testing json parse

--- assets/js/patientinfo.js
+++ assets/js/patientinfo.js
@@ -2,7 +2,9 @@ var xhttp = new XMLHttpRequest();
 var patientid = 1;
 xhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
-    alert(this.responseText);
+    var info = JSON.parse(this.responseText);
+    var infolist = 'patient_id: ' + info[0].patient_id + '\nuser_id: ' + info[0].user_id + '\nname: ' + info[0].first_name + ' ' + info[0].last_name + '\nbirthdate: ' + info[0].date_of_birth + '\naddress: ' + info[0].address + '\nmobile_number: ' + info[0].mobile_number;
+    alert(infolist);
 }
 };
 xhttp.open("POST", "assets/php/selectFromPatient.php", true);
