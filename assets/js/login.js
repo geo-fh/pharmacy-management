8260ddaa5cbd01dfc73e61e54f4c51402fe00298
geo-fh <georgesifarhat@gmail.com>
2022-06-20 10:54:16 +0300

    Testing login cookies

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,21 +2,24 @@ function validateLogin() {
   var xhttp = new XMLHttpRequest();
   var email = document.getElementById("InputEmail").value;
   var password = document.getElementById("InputPassword").value;
+  var usertype;
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       switch(this.responseText) {
         case 'pharmacist':
-          alert('pharmacist');
+          usertype = 1;
           break;
         case 'doctor':
-          alert('doctor');
+          usertype = 2;
           break;
         case 'patient':
-          alert('patient');
+          usertype = 3;
           break;
         default:
-          alert('not a user');
+          alert('Incorrect email or password');
+          usertype = 0;
       }
+      saveSession(email, usertype);
     }
   };
   xhttp.open("POST", "assets/php/login.php", true);
@@ -25,5 +28,6 @@ function validateLogin() {
 }
 
 function saveSession(email, usertype) {
-
+  document.cookie = "usertype=" + usertype;
+  document.cookie = "email=" + email;
 }
