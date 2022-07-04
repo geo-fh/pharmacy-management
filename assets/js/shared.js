1ba831d0611e0e8eb0184a8aa226228497f2a6e6
geo-fh <georgesifarhat@gmail.com>
2022-07-04 12:43:05 +0300

    made all pages visible for editing

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -34,11 +34,23 @@ function getDate(date) {
   return `${day}-${month}-${year}`;
 }
 
-populateSidebar('admin');
+populateSidebar('editor');
 
 function populateSidebar(usertype) {
   var r = new Array(), j = -1;
   switch (usertype) {
+    case 'editor':
+      r[++j] = "<li class=\"nav-item\"><a id=\"dashboardPage\" class=\"nav-link\" href=\"dashboard\"><i class=\"far fa-chart-bar\"></i><span>Statistics</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"indexPage\" class=\"nav-link\" href=\"index\"><i class=\"fas fa-home\"></i><span>Home</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"patientcarePage\" class=\"nav-link\" href=\"patientcare\"><i class=\"fas fa-user-circle\"></i><span>Patient Care</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"patientsummaryPage\" class=\"nav-link\" href=\"patientsummary\"><i class=\"fas fa-user-circle\"></i><span>Patient Summary</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"productlistPage\" class=\"nav-link\" href=\"productlist\"><i class=\"fas fa-bullseye\"></i><span>Products</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"profilePage\" class=\"nav-link\" href=\"profile\"><i class=\"fas fa-user\"></i><span>Profile</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"purchasehistoryPage\" class=\"nav-link\" href=\"purchasehistory\"><i class=\"fas fa-receipt\"></i><span>Purchase History</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"stockPage\" class=\"nav-link\" href=\"stock\"><i class=\"fas fa-bullseye\"></i><span>Stock</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"transactionlogPage\" class=\"nav-link\" href=\"transactionlog\"><i class=\"fas fa-receipt\"></i><span>Transaction Log</span></a></li>";
+      r[++j] = "<li class=\"nav-item\"><a id=\"usermanagementPage\" class=\"nav-link\" href=\"usermanagement\"><i class=\"fas fa-users\"></i><span>User Management</span></a></li>";
+      break;
     case 'pharmacist':
       r[++j] = "<li class=\"nav-item\"><a id=\"dashboardPage\" class=\"nav-link\" href=\"dashboard\"><i class=\"far fa-chart-bar\"></i><span>Statistics</span></a></li>";
       r[++j] = "<li class=\"nav-item\"><a id=\"patientcarePage\" class=\"nav-link\" href=\"patientcare\"><i class=\"fas fa-user-circle\"></i><span>Patient Care</span></a></li>";
